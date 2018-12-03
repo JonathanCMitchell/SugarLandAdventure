45da8374627ee09564c835281496912dac70b93d
Faith Yu <19146133+faithwyu@users.noreply.github.com>
2018-12-02 22:33:14 -0800

    modifications

--- model_scene.js
+++ model_scene.js
@@ -154,7 +154,7 @@ class model_scene extends Scene_Component {
 		this.model("lime.jpg",1);
 		this.model("cow.jpg",1);
 		this.model("ice.jpg",0.3);
-		this.model("ice1.png",1);
+		// this.model("ice1.png",0.5);
         this.submit_shapes(context, this.shapes);
 		this.materials["shadow"] = context.get_instance(Shadow_Shader)
 			.material(Color.of(0,0,0,1),
